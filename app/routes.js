18a16141b7dfc22f4d07c18b534c279463ae4acc
Max Karan <maxkaran2@gmail.com>
2018-08-10 12:59:51 -0400

    set up fontawesome, installed pizzapi, created checkout page

--- app/routes.js
+++ app/routes.js
@@ -4,18 +4,18 @@ var app = express();
 var router = express.Router();
 
 module.exports = function(app){
-    app.get('/', function(req, res){
+    app.get('/', function(req, res){ //index or home page
         res.render('index');
         console.log('Index page req');
     })
 
-    app.get('/test', function(req, res){
-        res.send('This is a test');
-        console.log('test page req');
-    })
-
-    app.get('/menu', function(req, res){
+    app.get('/menu', function(req, res){ //link to menu page, will parse menu through API to see what is available
         res.render('menu.ejs');
         console.log('Menu page req');
+    })
+
+    app.get('/checkout', function(req, res){ //link to checkout page, which will allow people to alter their cart of orders
+        res.render('checkout.ejs');
+        console.log('Checkout page req');
     })
 }
