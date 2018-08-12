f7865e96b65b241e4e98898bdb83498e83ccee95
Max Karan <maxkaran2@gmail.com>
2018-08-12 16:57:24 -0400

    instead of hardcoding store ID, it is searched for with address and closest store is returned

--- menu_test.js
+++ menu_test.js
@@ -1,10 +1,22 @@
 var pizzapi = require('dominos'); 
 var myStore = new pizzapi.Store({ID: 0});
 
-myStore.ID = 10310;
+//myStore.ID = 10310;
 
-myStore.getFriendlyNames(
+pizzapi.Util.findNearbyStores(
+    '399 Barrie St, Kingston, ON, k7k3t8',
+    'Delivery',
     function(storeData){
-        console.log(storeData);
+        console.log(storeData.result.Stores);
+        myStore.ID = storeData.result.Stores[0].StoreID;
+
+        myStore.getFriendlyNames(
+            function(storeData){
+                console.log(storeData);
+            }
+        );
     }
 );
+
+
+
